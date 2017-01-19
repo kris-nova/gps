3109054023e4f05c5c7717309172222b663b65a1
sam boyer <tech@samboyer.org>
2017-01-19 09:06:11 -0500

    Test for error from ListPackages on non-dir arg

--- analysis_test.go
+++ analysis_test.go
@@ -224,6 +224,16 @@ func TestWorkmapToReach(t *testing.T) {
 	}
 }
 
+func TestListPackagesNoDir(t *testing.T) {
+	out, err := ListPackages(filepath.Join(getwd(t), "_testdata", "notexist"), "notexist")
+	if err == nil {
+		t.Error("ListPackages should have errored on pointing to a nonexistent dir")
+	}
+	if !reflect.DeepEqual(PackageTree{}, out) {
+		t.Error("should've gotten back an empty PackageTree")
+	}
+}
+
 func TestListPackages(t *testing.T) {
 	srcdir := filepath.Join(getwd(t), "_testdata", "src")
 	j := func(s ...string) string {
@@ -249,7 +259,6 @@ func TestListPackages(t *testing.T) {
 					},
 				},
 			},
-			err: nil,
 		},
 		"code only": {
 			fileRoot:   j("simple"),
@@ -1078,7 +1087,7 @@ func TestExternalReach(t *testing.T) {
 	// There's enough in the 'varied' test case to test most of what matters
 	vptree, err := ListPackages(filepath.Join(getwd(t), "_testdata", "src", "github.com", "example", "varied"), "github.com/example/varied")
 	if err != nil {
-		t.Fatalf("listPackages failed on varied test case: %s", err)
+		t.Fatalf("ListPackages failed on varied test case: %s", err)
 	}
 
 	// Set up vars for validate closure
